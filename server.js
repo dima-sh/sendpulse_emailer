97e7a6d7b5de083be2b5a5d67cf40d689388e369
Dima Shemendiuk <shimdim@gmail.com>
2018-11-02 13:49:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require('path');
-var config = require('./config');
-var sendMail = require('./utils/sendmail');
-var app = express();
-var port = process.env.PORT || config.port;
-
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const sendMail = require('./utils/sendmail');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,13 +12,13 @@ app.use(bodyParser.json());
  * Routes
  * */
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.sendfile("./public/index.html");
+    res.sendfile('./public/index.html');
 });
-app.post('/sendmail',function(req,res){
+app.post('/sendmail', function(req,res){
     console.log('POST');
     res.setHeader('Access-Control-Allow-Origin', '*');
     sendMail(req.body, function(result){
@@ -28,6 +26,6 @@ app.post('/sendmail',function(req,res){
     });
 });
 
-app.listen(port,function(){
-  console.log('Started on PORT ' + port);
+app.listen(process.env.PORT, function(){
+  console.log('Started on PORT ' + process.env.PORT);
 });
